90d1a7689ca11b917530c72e3b3cbc8103cca3da
Deepali Shahapure <shahapuredeepali6@gmail.com>
2023-11-19 17:41:57 +0530

    EDA,model_training,exception,logger commit

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import find_packages,setup
+from typing import List
+
+
+
+setup(
+    name='Source',
+    version='0.0.1',
+    author='Deepa',
+    author_email="shahapuredeepali6@gmail.com",
+    install_requires='requirements.txt',
+    packages=find_packages()
+)
