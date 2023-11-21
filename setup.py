216b221d6f6e3e2eae2356a314a3829fdb34c65e
Deepali Shahapure <shahapuredeepali6@gmail.com>
2023-11-21 17:32:41 +0530

    all file added

--- setup.py
+++ setup.py
@@ -1,10 +1,11 @@
+
 from setuptools import find_packages,setup
 from typing import List
 
 
 
 setup(
-    name='Source',
+    name='src',
     version='0.0.1',
     author='Deepa',
     author_email="shahapuredeepali6@gmail.com",
